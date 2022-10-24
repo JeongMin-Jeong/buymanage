b17f985b6481e74120beef5a629ee00eb6ad581d
jihunjung7094 <letzzi@naver.com>
2022-10-24 16:56:00 +0900

    20221024-random_code_update_contract_register_list_button_add_done

--- src/main/resources/static/vendor/tools/custom_tools.js
+++ src/main/resources/static/vendor/tools/custom_tools.js
@@ -17,21 +17,45 @@
         }
 
         function displayRandomNum() {
-            //랜덤코드생성
+            //프로덕트랜덤코드생성
             //var rnum = Math.random();
             var rnum = randomString();
             var cdate = getYmd10();
             var pcode = "P" + cdate + rnum;
             document.getElementById("pcode").value = pcode;
             alert("프로덕트코드 생성 완료 하였습니다. : "+ pcode);
+            document.getElementById("pcode").disabled = true;
         }
 
         function displayRandomCNum() {
-            //랜덤코드생성
+            //계약랜덤코드생성
             //var rnum = Math.random();
             var rnum = randomString();
             var cdate = getYmd10();
             var ccode = "C" + cdate + rnum;
             document.getElementById("ccode").value = ccode;
             alert("계약코드 생성 완료 하였습니다. : "+ ccode);
+            document.getElementById("ccode").disabled = true;
+        }
+
+        function displayRandomSNum() {
+            //자재랜덤코드생성
+            //var rnum = Math.random();
+            var rnum = randomString();
+            var cdate = getYmd10();
+            var scode = "S" + cdate + rnum;
+            document.getElementById("scode").value = scode;
+            alert("자재코드 생성 완료 하였습니다. : "+ scode);
+            document.getElementById("scode").disabled = true;
+        }
+
+        function displayRandomONum() {
+            //발주랜덤코드생성
+            //var rnum = Math.random();
+            var rnum = randomString();
+            var cdate = getYmd10();
+            var ocode = "O" + cdate + rnum;
+            document.getElementById("ocode").value = ocode;
+            alert("발주코드 생성 완료 하였습니다. : "+ ocode);
+            document.getElementById("ocode").disabled = true;
         }
